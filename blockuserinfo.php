8d33086ed3152506b1241dc975ab9b286405a6a2
Markus Hitter <mah@jump-ing.de>
2018-06-14 17:57:34 +0200

    Make release 1.0.1.

--- blockuserinfo.php
+++ blockuserinfo.php
@@ -32,7 +32,7 @@ class BlockUserInfo extends Module
 	{
 		$this->name = 'blockuserinfo';
 		$this->tab = 'front_office_features';
-		$this->version = '1.0.0';
+		$this->version = '1.0.1';
 		$this->author = 'thirty bees';
 		$this->need_instance = 0;
 
